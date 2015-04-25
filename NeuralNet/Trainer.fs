5263a3d732092706decffc943b1e329f4ab56e24
info@millbank <in@magsolutionslimited.co.uk>
2015-04-25 12:32:22 +0100

    Adding the trainer

---
+++ NeuralNet/Trainer.fs
@@ -0,0 +1,22 @@
+﻿namespace Training
+open System
+open Perceptron
+module Trainer =
+    
+    let train r inputs expected numbers limit =
+        let weights = Perceptor.generateRandomWeights r numbers limit
+        let guess = Perceptor.feedForward inputs weights
+        let error = expected - guess
+        (0.0, inputs, weights)
+        |||> List.fold2 (fun s i w -> s + (w + Perceptor.constant * error * i))
+
+    let trainer x y a =
+        (x,y,1,a)
+
+    let calculateY x =
+        2*x+1
+        
+    let yLine x = calculateY x
+
+    let evaluatePoint y line =
+        if y < line then -1 else 1
